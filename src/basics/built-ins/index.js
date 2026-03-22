a6298376080ec890b6a135932e22c8fa4efb39a2
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-03-22 16:59:22 +0200

    feat: Add built-ins example and enable strict TypeScript compiler options for `noImplicitReturns`, `noUnusedLocals`, and `noUnusedParameters`.

--- src/basics/built-ins/index.js
+++ src/basics/built-ins/index.js
@@ -1,7 +1,3 @@
-function calculateTax(income) {
-    if (income < 50000)
-        return income * 1.2;
-    return undefined;
-}
-console.log(calculateTax(30000)); // should return a number
-console.log(calculateTax(100000)); // should return undefined
+var employee = { id: 1 };
+employee.name = "Elisha";
+console.log(employee);
--- src/basics/built-ins/index.ts
+++ src/basics/built-ins/index.ts
@@ -1,7 +1,4 @@
-function calculateTax(income: number): (number | undefined) {
-  if (income < 50_000)
-    return income * 1.2
-  return undefined
-}
+let employee = {id: 1};
+employee.name = "Elisha";
 
-console.log(calculateTax(100_000)); // should return undefined
+console.log(employee)
